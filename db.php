585df082a8286af36bfa5f3cfba0cea6803d8401
TriumfM <triumf_96@hotmail.com>
2017-05-03 16:34:11 +0200

    db nga triumf

--- db.php
+++ db.php
@@ -1,13 +1,13 @@
  <?php
 
-   $host        = "host = 127.0.0.1";
-   $port        = "port = 5432";
-   $dbname      = "dbname = AirlineReservation";
-   $credentials = "user = postgres password=fiek123456";
-
-   $db = pg_connect( "$host $port $dbname $credentials"  );
-   if(!$db) {
-      echo "Error : Unable to open database\n";
-   }
+ //  $host        = "host = 127.0.0.1";
+ //  $port        = "port = 5432 ";
+    $dbname      = "dbname = test1";
+ //  $credentials = "user = postgres password =6991";
+ //
+  $db = pg_connect( "$dbname" );
+ //  if(!$db) {
+ //     echo "Error : Unable to open database\n";
+ //  }
 
   ?>
