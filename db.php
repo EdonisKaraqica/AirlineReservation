7f5662a37151b83f1e014ee0acbf5930020c2498
Eriona Osmani <Eriona Osmani>
2017-05-03 16:38:50 +0200

    mos e fshij

--- db.php
+++ db.php
@@ -3,11 +3,18 @@
    $host        = "host = 127.0.0.1";
    $port        = "port = 5432";
    $dbname      = "dbname = AirlineReservation";
-   $credentials = "user = postgres password=fiek123456";
+   $credentials = "user = postgres password=pass123";
 
    $db = pg_connect( "$host $port $dbname $credentials"  );
    if(!$db) {
       echo "Error : Unable to open database\n";
    }
+   else {
+   	echo "succed";
+   }
+
+   $query = pg_query($db, "SELECT * FROM works ORDER BY id"); 
+
+   print $query;
 
   ?>
