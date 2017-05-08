177d16e9c85afde2a16d7c23cf05fda9610a4165
TriumfM <triumf_96@hotmail.com>
2017-05-08 11:10:48 +0200

    menu init

---
+++ menu/head_menu.php
@@ -0,0 +1,20 @@
+<?php  ?>
+
+<head>
+		<title>AirlineReservation</title>
+		<link rel="stylesheet" type="text/css" href="menu.css">
+
+</head>
+<body>
+	
+	<ul class="menu">	
+		<li a="home.php">Home</li>
+		<li a="info.php">Info</li>
+		<li>Rezervo</li>
+		<li>Orai i fluturimeve</li>
+	</ul>
+
+
+
+
+</body>
