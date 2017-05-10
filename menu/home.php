435e2941246f84bf606f4bcc8422e79b2e081bf5
Eriona Osmani <Eriona Osmani>
2017-05-10 14:58:36 +0200

    info.php & home.php

--- menu/home.php
+++ menu/home.php
@@ -16,19 +16,84 @@ w3IncludeHTML();
 
 <style>
 
-/*#slideshow {float: left;border-style: solid;width: 80%;position: relative; }
+#homepage {float: left;border-style: solid;width: 100%;position: relative; }
 
-.photos{border-style : solid ; width : 80%;height : 50% ; position: fixed; left :10% ; top :20%; position: fixed;}
+.slideshow{border-style: solid;height:45%;width:90%;position: fixed; left:5%; top :12%;}
+
+.pic1 {border-style: solid;height:25%;width:28.5%;position: fixed; left:5%; top :60%;}
+
+.pic2 {border-style: solid;height:25%;width:28.5%;position: fixed; left:35.75%; top :60%;}
+
+.pic3 {border-style: solid;height:25%;width:28.5%;position: fixed; left:66.5%; top :60%;}
 
 
-<div id="slideshow">
-	<div class="photos">
-		<img src="http://www.clipartbest.com/cliparts/aTq/zgA/aTqzgAzXc.png" alt="Mountain View" style="width:50px;height:50px;margin:10px">
 
+</style>
+</head>
+<body>
+
+<div id="homepage">
+<div class="slideshow">
+
+<div class="mySlides fade">
+   <img src="air.jpg" style="width:50%;height: 50%">
+</div>
+
+<div class="mySlides fade">
+  <img src="air.jpg" style="width:28.5%;height: 45%">
+</div>
+
+<div class="mySlides fade">
+  <img src="http://wallpapercave.com/wp/7Unu9SU.jpg" style="width:28.5%;height: 45%">
+</div>
+
+</div>
+
+	<div class="pic1">
+		  <img src="http://wallpapercave.com/wp/7Unu9SU.jpg" style="width:100%;height: 100%;object-fit: contain">
 	</div>
 
+	<div class="pic2">
+		<img src="http://wallpapercave.com/wp/7Unu9SU.jpg" style="width:100%;height: 100%;object-fit: contain">
+	</div>
 
-</div>*/
+	<div class="pic3">
+		<img src="http://wallpapercave.com/wp/7Unu9SU.jpg" style="width:100%;height: 100%;object-fit: contain">
+	</div>
+
+</div>
+
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
+
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+       slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex> slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 2000); 
+}
+</script>
+
+
 
 
 </style>
