1c46cb722c61592076e47e406693286528220452
mentorelshani <mentorelshani04@gmail.com>
2017-05-23 10:46:03 +0200

    Add files via upload

--- adminpage.php
+++ adminpage.php
@@ -1,27 +1,35 @@
 <?php 
-include "head_menu.php";
+	//include "header.php";
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<script src="https://www.w3schools.com/lib/w3data.js"></script>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.min.js"></script>
+<script src="adminflights.js"></script>
+
 
 <body>
 
 
 <style>
 
-#adminpage {float: left;border-style: solid;width: 100%;position: relative; z-index: -1;}
+#adminpage {float: left;;width: 100%;position: relative; z-index: -1;}
 .admin {}
 
 .links {height:80%;width:15%;position: fixed; left:5%; top :12%;}
 
-.table {border-style: solid; height:80%;width:76%;position: fixed; left:22%; top :12%;}
+.tabela {height:80%;width:76%;position: fixed; left:22%; top :12%;}
+
 
 table, th, td {
     border: 1px solid black;
     border-collapse: collapse;
+    text-align: center;
 }
 
 div.vertical-line{
@@ -43,12 +51,48 @@ div.vertical-line{
 <div id="adminpage">
 
 	<div class="links">
-		<label></label> <input type="text" name="flight" > 
-        <input type="submit" name="submit" value="Search flight"> 
-		<p>*<a href="#">All flights</a></p>
-		<p>*<a href="#">All reservations</a></p>
-		<p>*<a href="#">All users</a></p>
-		<p>*<a href="#">Add flight</a></p>
+
+	<form id=#search>
+ <label>Search by:</label> <input list="dataType" name="dataType">
+  <datalist id="dataType">
+    <option value="Route Code">
+    <option value="Airporti">
+    <option value="Destinacioni">
+  </datalist>
+
+
+<br>
+
+
+		<label>Value:   </label> <input type="text" name="value" > 
+        <input type="submit" name="submit" value="Search flight"> <br><br>
+</form>
+
+
+
+  	<p><input type="button" id="flights" value="All flights" /></p>
+
+	<p><input type="button" id="reservations" value="All reservations" /></p>
+
+	<p><input type="button" id="users" value="All users" /></p>
+
+	<p><input type="button" id="addFlight" value="Add flight" /></p>
+
+	<p><input type="button" id="deleteflight" value="Delete flight" /></p>
+
+
+<p> User ID:<input id="asd"  type="text"></p>
+
+<p> Username:<input id="qwe"  type="text"></p>
+
+
+
+
+
+		<p>*<a href="#flights">All flights</a></p>
+		<p>*<a href="#reservationsTable">All reservations</a></p>
+		<p>*<a href="#usersTable">All users</a></p>
+		<p>*<a href="#addFlight">Add flight</a></p>
 		<p>*<a href="#">Update flight</a></p>
 		<p>*<a href="#">Delete flight</a></p>
 	</div>
@@ -57,28 +101,14 @@ div.vertical-line{
 
 	</div>
 
-	<div class="table">
-				<div class="container">           
-  			<table style="width: 100%">
-   		 <thead>
-      <tr>
-      	<th>Data</th>
-        <th>Destinacioni</th>
-        <th>Nisja</th>
-        <th>Arritja</th>
-        <th>ID e fluturimi</th>
-
-      </tr>
-    	</thead>
+				<div class="container"> 
+	<div class="tabela">          
+	<table  class = "table table-bordered" style="width: 100%" >
 
   </table>
 </div>
 	</div>
 
-
 </div>
 
-<?php
-include "footer.php";
-?>
 </body>
