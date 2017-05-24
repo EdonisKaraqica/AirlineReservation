ab8b1e4dc3b28c2be9599590b3c31d8f21424a04
mentorelshani <mentorelshani04@gmail.com>
2017-05-24 13:06:55 +0200

    Add files via upload

--- flights.php
+++ flights.php
@@ -1,11 +1,11 @@
  <?php
 
- // if(function_exists($_GET['f'])) {
- //  $_GET['f']();
- // }
- // else{
- // 	echo "Nuk eksiston";
- // }
+ if(function_exists($_GET['f'])) {
+  $_GET['f']();
+ }
+ else{
+ 	echo "Nuk eksiston";
+ }
 
  function getFlights(){ 
 
@@ -59,16 +59,23 @@
  function getReservations(){ 
 
 
+
     require 'db.php';
 
-     $query = pg_query($db, "SELECT
-      reservationID, username , data_fluturimit , nisja , mberritja , airoporti , destinacioni , routecode ,qmimi
-    FROM route as ro
-    inner join qmimi_tiketave as qt on qt.id_route = ro.rtid
-    inner join orari_fluturimeve as o on o.cmimi_id=qt.id_qmimi
-    inner join reservations as re on re.fluturimi = o.id_fluturimi
-    inner join users as us on us.userId = re.userId
-    ORDER BY reservationID ");
+
+   $sql ="SELECT
+        reservationID, username , data_fluturimit , nisja , mberritja , airoporti , destinacioni , routecode ,qmimi
+     FROM route as ro
+     inner join qmimi_tiketave as qt on qt.id_route = ro.rtid
+     inner join orari_fluturimeve as o on o.cmimi_id=qt.id_qmimi
+     inner join reservations as re on re.fluturimi = o.id_fluturimi
+     inner join users as us on us.userId = re.userId
+     ORDER BY reservationID ";
+
+
+
+
+     $query = pg_query($db, $sql);
 
      $result=pg_fetch_all($query);
 
@@ -88,50 +95,45 @@
        $result = pg_query($db, "INSERT INTO users(id, username) VALUES('  $id  ', '  $username  ')");
 }
 
- function findUser(){ 
+ function searchBy() { 
+
+   require 'db.php';   
 
 
-
-   require 'db.php';
-
-      if (!isset($id)){
-        $id = pg_escape_string($_POST['id']);
-
-        }
-
+         $dataType = $_POST['dataType'];
+         $value = $_POST['value'];
       
-      else {
-          $query = pg_query($db, "SELECT username FROM users where id = $id ");
+      
+         $query = pg_query($db, "SELECT
+          data_fluturimit , nisja , mberritja , airoporti , destinacioni , routecode , qmimi 
+         FROM route as ro
+         inner join qmimi_tiketave as qt on qt.id_route=ro.rtid
+         inner join orari_fluturimeve as o on o.cmimi_id=qt.id_qmimi
+         WHERE $dataType LIKE '%$value%' ");
       
 
           $result=pg_fetch_all($query);
 
 
          print_r(json_encode($result));
-      }
+       
+      
 }
 
-function searchBy(){
+// function searchBy(){
 
-  require 'db.php';
+//   require 'db.php';
 
-$id=pg_query($db, ("SELECT MAX (id) FROM Users"));
-$result=pg_fetch_all($id);
-console_log ($result);
+//   $dataType = pg_escape_string($_POST['dataType'])
 
-
-  // $dataType = pg_escape_string($_POST['dataType'])
-  // echo $id;
-  // $query = pg_query($db,"SELECT
-  //     reservationID, username , data_fluturimit , nisja , mberritja , airoporti , destinacioni , routecode ,qmimi
-  //   FROM route as ro
-  //   inner join qmimi_tiketave as qt on qt.id_route = ro.rtid
-  //   inner join orari_fluturimeve as o on o.cmimi_id=qt.id_qmimi
-  //   inner join reservations as re on re.fluturimi = o.id_fluturimi
-  //   inner join users as us on us.userId = re.userId
-  //   WHERE $dataType = $value " )
-}
-
-
+//   $query = pg_query($db,"SELECT
+//       reservationID, username , data_fluturimit , nisja , mberritja , airoporti , destinacioni , routecode ,qmimi
+//     FROM route as ro
+//     inner join qmimi_tiketave as qt on qt.id_route = ro.rtid
+//     inner join orari_fluturimeve as o on o.cmimi_id=qt.id_qmimi
+//     inner join reservations as re on re.fluturimi = o.id_fluturimi
+//     inner join users as us on us.userId = re.userId
+//     WHERE $dataType = $value " )
+// }
 
 ?>
