41bdc6ac672851de82c432f4148d549e1ceb6b6a
TriumfM <triumf_96@hotmail.com>
2017-05-29 10:43:43 +0200

    Add files via upload

---
+++ info_paraUdhetimit.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+	<head>
+<style>
+
+
+		div#content 
+		{
+    		
+    		padding: 10px;
+    		background-color: lightgrey;
+    		position: relative;
+		}
+
+		div.article 
+		{
+   			margin: 5px;
+    		padding: 10px;
+    		background-color: transparent;
+    		width:40%;
+    		position: absolute;
+		}
+
+        div#div_permbajtja_para_udhetimiet
+        {
+            margin-top: 100px;
+        }
+
+
+</style>
+		
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">	
+	<!-- Login javascript button -->
+	<link rel="stylesheet" type="text/css" href="login.css">
+    <!-- Custom CSS -->
+    <link href="css/modern-business.css" rel="stylesheet">
+    <!-- Custom Fonts -->
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	</head>
+
+	<body>
+
+	<?php  include 'header.php' ?>
+		<div id="menu">
+		
+		</div>
+
+		<div class="container">
+
+
+        <!-- Portfolio Section -->
+
+
+        <div id="div_permbajtja_para_udhetimiet">
+        		<h2>Para udhëtimit</h2>
+                <br>
+                <h4>Check-in</h4>
+				<p>U rekomandohet pasagjerëve që, për fluturime me Malësia Reisen, mundësisht të paraqiten për përgatitje për fluturim dy orë përpara fluturimit të planifikuar, sepse sportelet mbyllen 30 minuta përpara nisjes.</p>
+                <br>
+
+        	<h4>Dokumentet e udhëtimit</h4>
+                <p>Pasagjeri është personalisht përgjegjës që të ketë me vete dokumente të vlefshme identifikimi, të cilat u përgjigjen rregullave të SWISS dhe të Malësia Reisen si dhe rregullave për hyrje në vend të secilit cak të fluturimit. Gjobat, dënimet me të holla, pagesat ose harxhimet e çdo lloji si pasojë e mospërfilljes së këtyre rregullave, duhet të barten nga vetë pasagjeri. Për të siguruar respektimin e rregullave, Malësia Reisen rekomandon që për të gjitha fluturimet të bartet me vete pasaporta e vlefshme (dhe, nëse është e nevojshme, edhe viza e vlefshme). </p>
+                <br>
+                <h4>Foshnjat dhe shoqërimi</h4>
+                <p>Në qoftë se fëmijët udhëtojnë të pashoqëruar, atëherë vlejnë rregullat ndërkombëtare për të ashtuquajturit „Unaccompanied Minors (UM)“, në mënyrë që edhe pasagjerët e vegjël të mbërrijnë të sigurt në cakun e tyre. Për një udhëtim të tillë special, përgatituni me kujdes së bashku me fëmijën ose fëmijët tuaj. Udhëzim: për fëmijë të pashoqëruar nuk është e mundur të bëhet rezervimi online. </p>
+                <br>
+
+                <h4>SWISS i definon fëmijët e pashoqëruar si më poshtë: </h4>
+                <ul>
+                    <li>Fëmijë midis moshës 5 dhe 12-vjeçare; pa person shoqërues prej moshës 15-vjeçare</li>
+                    <li>Fëmijë midis moshës 3 dhe 5-vjeçare; me person shoqërues midis moshës 12 deri në 15-vjeçare</li>
+                </ul>
+                <p>Sipas dëshirës së prindërve përkujdesja mund të bëhet edhe për fëmijët midis moshës 12 dhe 18-vjeçare. Rezervimi i fluturimit është i mundur të bëhet vetëm nga prindërit ose nga personi që e pret në aeroport arritjen e fëmijës. Përgatisni të gjitha dokumentet e nevojshme për udhëtim. </p>
+                <br>
+
+        </div>
+        <hr>
+        <div>
+            <h2>Bagazhi</h2>
+                <br>
+                <h4>Bagazhi i pataksuar</h4>
+                <p>Kufiri i bagazhit të pataksuar për bagazhin e dorëzuar është 20 kg për pasagjer. Edhe fëmijët kanë të drejtë të kenë këtë peshë bagazhi. Ju rekomandojmë që të mos dorëzoni bagazhe të veçanta, të cilat kanë peshë më të rëndë se 28 kg. </p>
+                <br>
+
+            <h4>Mbipesha e bagazhit</h4>
+                <p>Transportimi i bagazhit, i cili për nga pesha i tejkalon kufijtë e përcaktuar të bagazhit të pataksuar, bëhet me pagesë. 
+</p>
+                <br>
+                <h4>Bagazhi i dorës</h4>
+                <p>Për shkaqe sigurie si dhe për shkak të rehatisë, në kabinë lejohet të merret vetëm një copë bagazhi dore për person. Bagazhi i dorës guxon të ketë peshën maksimale 6 kg (8 kg për laptop), derisa përmasat nuk guxojnë të tejkalojnë 55 cm x 40 cm x 20 cm. </p>
+                <br>
+                <p>Është e këshillueshme që në bagazhin e dorës të transportohen gjësendet me vlerë, medikamentet si dhe gjësendet që prishen lehtë ose që thyhen. Nga ana tjetër vlejnë udhëzimet rreth kufizimeve të përgjegjësisë, të renditura në vërtetimin e rezervimit të biletës. </p>
+                <br>
+                <h4>Tarifa e mbipeshës së bagazhit </h4>
+                <p>Pesha më e lartë e lejuar e bagazhit tuaj të udhëtimit është e shënuar në biletën tuaj të fluturimit. Tejkalimet e këtyre limiteve konsiderohen si mbipeshë bagazhi. Në rast të këtillë, struktura tarifore kërkon një çmim shtesë. Ky çmim shtesë përcaktohet varësisht nga situata e konceptit të peshës apo numrit të pjesëve të bagazhit.  </p>
+                <br>
+                <h4>Dëmtimi i bagazhit dhe humbja e bagazhit</h4>
+                <p>Numrin më të madh të bagazheve të drejtuar gabimisht i gjejmë brenda 24 orësh. Mund të ndodh në raste të rralla që bagazhi të mos mund të gjendet as brenda pesë ditësh. Pastaj, zyra përkatëse „Lost & Found” fillon procedurën e dytë, të detajizuar të kërkimit. Në qoftë se bagazhi juaj i humbur nuk gjendet brenda pesë ditësh ose nëse ju keni pyetje në lidhje me bagazhin e drejtuar gabimisht, ju lutemi ta kontaktoni menjëherë qendrën tonë të kërkimit të bagazheve në Prishtinë:</p>
+                <br>
+                <address>
+                    Prishtina Central Baggage<br>
+                    Tracing Office<br>
+                    ALB-3123 Prishtine<br>
+                    Telefon +383 44 32 32 32 <br>
+                    Faks +383 38 31 96 23<br>
+                </address>
+            
+        </div>
+
+		<div id="footer">
+			<!-- <?php include 'footer'; ?> -->
+		</div>
+
+	</body>
+
+</html>
